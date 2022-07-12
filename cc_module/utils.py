dff04f407c44457fa585de18c9e06dbd1f96b888
MorganMalavasi <morgan.malavsi@gmail.com>
2022-07-12 15:35:11 +0000

    beta 1.0

--- cc_module/utils.py
+++ cc_module/utils.py
@@ -41,3 +41,41 @@ def truncatefloat6digits(n):
 def trunc(a, x):
     int1 = int(a * (10**x))/(10**x)
     return float(int1)
+	
+
+def plot_blobs(X, labels=None, threeD=False, doPCA=True, sizex=1):
+
+	#plt.figure(figsize=(10, 10))
+	#plt.scatter(X[:, 0], X[:, 1], c=l)
+	#plt.title("Blobs")
+	#plt.show()
+
+	if threeD:
+		if PCA:
+			pca = PCA(n_components=3)
+			components = pca.fit_transform(X)
+		else:
+			components = X[:,0:3]  
+		if labels is None:
+			fig = px.scatter_3d(components, x=0, y=1, z=2, title='Blobs 3D', labels={'0': 'PC 1', '1': 'PC 2', '2': 'PC 3'})
+		else:
+			fig = px.scatter_3d(components, x=0, y=1, z=2, color=labels, title='Blobs 3D', labels={'0': 'PC 1', '1': 'PC 2', '2': 'PC 3'})
+	else:
+		if doPCA:
+			pca = PCA(n_components=2)
+			components = pca.fit_transform(X)
+		else:
+			components = X[:,0:2]  
+		if labels is None:
+			fig = px.scatter(components, x=0, y=1, title='Blobs 2D', labels={'0': 'PC 1', '1': 'PC 2'})
+		else:
+			fig = px.scatter(components, x=0, y=1, title='Blobs 2D', color=labels, labels={'0': 'PC 1', '1': 'PC 2'})
+	
+	fig.update_layout(
+		width = 800*sizex,
+		height = 800*sizex,
+		title = "fixed-ratio axes")
+	fig.update_yaxes(
+		scaleanchor = "x",
+		scaleratio = 1)
+	fig.show()  
